90f043c7b4f181b86553372601ffd2fb030219f7
Massamba Sall <sallmassamba36@gmail.com>
2020-11-30 13:44:15 +0100

    register service worker

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import register from './registerServiceWorkers';
+import registerServiceWorker from './registerServiceWorker';
 import {searchRobots, requestRobots} from './reducers'
 import 'tachyons';
 
@@ -28,5 +28,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+registerServiceWorker();
 reportWebVitals();
-register();
+
